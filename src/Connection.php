ffe9b7c05872f9f9b0b5dfa2ad0420f6a97dc225
Paul Goodchild <paul@icontrolwp.com>
2017-07-17 20:53:22 +0100

    remove the constants file

--- src/Connection.php
+++ src/Connection.php
@@ -3,4 +3,18 @@
 namespace FernleafSystems\Apis\Email\Mailchimp;
 
 class Connection extends \FernleafSystems\Apis\Base\Connection {
+
+	/**
+	 * @return string
+	 */
+	public function getBaseUrl() {
+		return 'https://%s.api.mailchimp.com/3.0/';
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getContentType() {
+		return 'application/vnd.api+json';
+	}
 }
