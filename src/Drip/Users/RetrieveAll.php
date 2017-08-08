1f8ad63d568fea5b3a986fc6531480b283ed1bf9
Paul Goodchild <paul@icontrolwp.com>
2017-08-08 10:46:22 +0100

    fix for retrieveall class name

--- src/Drip/Users/RetrieveAll.php
+++ src/Drip/Users/RetrieveAll.php
@@ -4,7 +4,11 @@ namespace FernleafSystems\Apis\Email\Drip\Users;
 
 use FernleafSystems\Apis\Email\Drip;
 
-class Retrieve extends Drip\Api {
+/**
+ * Class RetrieveAll
+ * @package FernleafSystems\Apis\Email\Drip\Users
+ */
+class RetrieveAll extends Drip\Api {
 
 	const REQUEST_METHOD = 'get';
 
