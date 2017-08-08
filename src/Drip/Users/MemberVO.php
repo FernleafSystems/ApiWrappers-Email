b33e2440798766afae1fd522c3dddbf27aaf4c08
Paul Goodchild <paul@icontrolwp.com>
2017-08-08 13:14:42 +0100

    added retrieveall and membervo for drip

---
+++ src/Drip/Users/MemberVO.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace FernleafSystems\Apis\Email\Drip\Users;
+
+use FernleafSystems\Utilities\Data\Adapter\StdClassAdapter;
+
+/**
+ * Class MemberVO
+ * @package FernleafSystems\Apis\Email\Drip\Users
+ */
+class MemberVO {
+
+	use StdClassAdapter;
+
+	/**
+	 * @return string
+	 */
+	public function getCreatedAt() {
+		return $this->getStringParam( 'created_at' );
+	}
+
+	/**
+	 * @param string $sFieldId
+	 * @return mixed|null
+	 */
+	public function getCustomField( $sFieldId ) {
+		$aFields = $this->getCustomFields();
+		return isset( $aFields[ $sFieldId ] ) ? $aFields[ $sFieldId ] : null;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getCustomFields() {
+		return $this->getArrayParam( 'custom_fields' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEmail() {
+		return $this->getStringParam( 'email' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getId() {
+		return $this->getStringParam( 'id' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getStatus() {
+		return $this->getStringParam( 'status' );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getTags() {
+		return $this->getArrayParam( 'tags' );
+	}
+
+	/**
+	 * @param string $sTag
+	 * @param bool   $bCaseSensitive
+	 * @return bool
+	 */
+	public function hasTag( $sTag, $bCaseSensitive = true ) {
+		return in_array(
+			$bCaseSensitive ? $sTag : strtolower( $sTag ),
+			$bCaseSensitive ? $this->getTags() : array_map( 'strtolower', $this->getTags() )
+		);
+	}
+}
--- src/Drip/Users/RetrieveAll.php
+++ src/Drip/Users/RetrieveAll.php
@@ -13,6 +13,34 @@ class RetrieveAll extends Drip\Api {
 	const REQUEST_METHOD = 'get';
 
 	/**
+	 * @return MemberVO[]
+	 */
+	public function retrieve() {
+		$aAllMembers = array();
+
+		$nPage = 1;
+		do {
+			$aMembers = $this->setRequestDataItem( 'page', $nPage++ )
+							 ->setRequestDataItem( 'per_page', 1000 )
+							 ->send()
+							 ->getDecodedResponseBody();
+
+			$bHasResults = !empty( $aMembers[ 'subscribers' ] ) && is_array( $aMembers[ 'subscribers' ] );
+			if ( $bHasResults ) {
+				$aMembers = array_map(
+					function ( $aMember ) {
+						return ( new MemberVO() )->applyFromArray( $aMember );
+					},
+					$aMembers[ 'subscribers' ]
+				);
+				$aAllMembers = array_merge( $aAllMembers, $aMembers );
+			}
+		} while ( $bHasResults );
+
+		return $aAllMembers;
+	}
+
+	/**
 	 * @return string
 	 */
 	protected function getUrlEndpoint() {
