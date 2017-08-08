fbc023cead8664aa1781e92f50a8d81e8ac73a38
Paul Goodchild <paul@icontrolwp.com>
2017-08-08 11:56:43 +0100

    allow raw content to be passed to webhook capture

--- src/Common/Webhooks/Capture.php
+++ src/Common/Webhooks/Capture.php
@@ -10,12 +10,15 @@ class Capture {
 
 	/**
 	 * Uses 'php://input' to capture the raw webhook data
+	 * @param string $sRawContent
 	 * @return WebhookVO
 	 */
-	public function captureFromInput() {
+	public function captureFromInput( $sRawContent = null ) {
 		$oWebhook = new WebhookVO();
 
-		$sRawContent = file_get_contents( 'php://input' );
+		if ( is_null( $sRawContent ) ) {
+			$sRawContent = file_get_contents( 'php://input' );
+		}
 		if ( !empty( $sRawContent ) && is_string( $sRawContent ) ) {
 			$aDecoded = json_decode( $sRawContent, true );
 			if ( !empty( $aDecoded ) && is_array( $aDecoded ) ) {
