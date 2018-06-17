3a6be23e5aaf13e95011f6d31e934f63a1ec43f5
Paul G <paul@icontrolwp.com>
2018-06-17 11:17:45 +0100

    fill out sib account vo

--- src/SendInBlue/Account/AccountVO.php
+++ src/SendInBlue/Account/AccountVO.php
@@ -15,7 +15,63 @@ class AccountVO {
 	/**
 	 * @return string
 	 */
-	public function getName() {
-		return $this->getStringParam( 'name' );
+	public function getAddress() {
+		return $this->getStringParam( 'address' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getClientId() {
+		return $this->getParam( 'client_id' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEmail() {
+		return $this->getStringParam( 'email' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFirstName() {
+		return $this->getStringParam( 'first_name' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLastName() {
+		return $this->getStringParam( 'last_name' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCity() {
+		return $this->getStringParam( 'city' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCompanyName() {
+		return $this->getStringParam( 'company' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCountry() {
+		return $this->getStringParam( 'country' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPostalCode() {
+		return $this->getStringParam( 'zip_code' );
 	}
 }
