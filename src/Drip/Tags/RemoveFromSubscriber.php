14874474b12365d5db55366b98979df8e3de4039
Paul G <paul@icontrolwp.com>
2018-06-19 12:35:01 +0100

    provide ability to apply and remove tags from users

---
+++ src/Drip/Tags/RemoveFromSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace FernleafSystems\ApiWrappers\Email\Drip\Tags;
+
+use FernleafSystems\ApiWrappers\Email\Drip;
+
+/**
+ * Class RemoveFromSubscriber
+ * @package FernleafSystems\ApiWrappers\Email\Drip\Tags
+ */
+class RemoveFromSubscriber extends Drip\Api {
+
+	const REQUEST_METHOD = 'delete';
+
+	/**
+	 * @param string $sEmail
+	 * @return $this
+	 */
+	public function setEmail( $sEmail ) {
+		return $this->setParam( 'email', $sEmail );
+	}
+
+	/**
+	 * @param string $sTag
+	 * @return $this
+	 */
+	public function setTag( $sTag ) {
+		return $this->setParam( 'tag', $sTag );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getUrlEndpoint() {
+		return sprintf( 'subscribers/%s/tags/%s',
+			rawurlencode( $this->getParam( 'email' ) ),
+			rawurlencode( $this->getParam( 'tag' ) )
+		);
+	}
+}
