bff1b464ed668e698bd87baac09449e8396344b7
Paul G <paul@icontrolwp.com>
2018-06-20 10:50:27 +0100

    add helpers to drip webhook vo

--- src/Drip/Webhooks/WebhookVO.php
+++ src/Drip/Webhooks/WebhookVO.php
@@ -77,4 +77,62 @@ class WebhookVO extends \FernleafSystems\ApiWrappers\Email\Common\Webhooks\Webho
 	public function getEvent() {
 		return $this->getStringParam( 'event' );
 	}
+
+	/**
+	 * https://developer.drip.com/#webhook-events
+	 * @param string $sEvent
+	 * @return bool
+	 */
+	public function isEvent( $sEvent ) {
+		return $this->getEvent() == $sEvent;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isEvent_AppliedTag() {
+		return $this->isEvent( 'subscriber.applied_tag' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isEvent_Deleted() {
+		return $this->isEvent( 'subscriber.deleted' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isEvent_PerformedEvent() {
+		return $this->isEvent( 'subscriber.performed_custom_event' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isEvent_RemovedFromCampaign() {
+		return $this->isEvent( 'subscriber.removed_from_campaign' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isEvent_RemovedTag() {
+		return $this->isEvent( 'subscriber.removed_tag' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isEvent_UnsubscribedAll() {
+		return $this->isEvent( 'subscriber.unsubscribed_all' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isEvent_UnsubscribedCampaign() {
+		return $this->isEvent( 'subscriber.unsubscribed_from_campaign' );
+	}
 }
