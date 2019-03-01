93ad260a5a7456ce23367ac522e71f9a37c5a383
Paul G <paul@icontrolwp.com>
2019-03-01 14:48:06 +0000

    add abiliyt to delete ActiveCampaign contact

---
+++ src/ActiveCampaign/Contacts/Delete.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace FernleafSystems\ApiWrappers\Email\ActiveCampaign\Contacts;
+
+/**
+ * Class Delete
+ * @package FernleafSystems\ApiWrappers\Email\ActiveCampaign\Contacts
+ */
+class Delete extends Base {
+
+	const REQUEST_METHOD = 'delete';
+
+	/**
+	 * @param string $sEmail
+	 * @return bool
+	 */
+	public function byEmail( $sEmail ) {
+		$oContact = ( new Retrieve() )
+			->setConnection( $this->getConnection() )
+			->byEmail( $sEmail );
+		return ( $oContact instanceof ContactVO ) ? $this->byId( $oContact->getId() ) : false;
+	}
+
+	/**
+	 * @param string $sId
+	 * @return bool
+	 */
+	public function byId( $sId ) {
+		return $this->setParam( 'id', $sId )
+					->req()
+					->isLastRequestSuccess();
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getUrlEndpoint() {
+		return sprintf( '%s/%s', parent::getUrlEndpoint(), $this->getParam( 'id' ) );
+	}
+}
