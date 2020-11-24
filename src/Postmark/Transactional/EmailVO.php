0297e7451ba637842945501d4cb3f518fc3d0425
Paulie G <paul@icontrolwp.com>
2020-11-24 11:21:14 +0000

    fixes for postmark

--- src/Postmark/Transactional/EmailVO.php
+++ src/Postmark/Transactional/EmailVO.php
@@ -9,4 +9,23 @@ namespace FernleafSystems\ApiWrappers\Email\Postmark\Transactional;
  */
 class EmailVO extends \FernleafSystems\ApiWrappers\Email\Common\Transactional\EmailVO {
 
+	public const TRACK_LINKS_NONE = 'None';
+	public const TRACK_LINKS_BOTH = 'HtmlAndText';
+	public const TRACK_LINKS_HTML = 'HtmlOnly';
+	public const TRACK_LINKS_TEXT = 'TextOnly';
+
+	public function getTrackLinks() :string {
+		if ( isset( $this->track_links ) && $this->track_links !== false ) {
+			if ( $this->track_links === true ) {
+				$trk = self::TRACK_LINKS_BOTH;
+			}
+			else {
+				$trk = $this->track_links;
+			}
+		}
+		else {
+			$trk = self::TRACK_LINKS_NONE;
+		}
+		return $trk;
+	}
 }
